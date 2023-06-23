b28a4a4f029e5c00e2c0251948ed498560373d29
Jake Spievak <jakespvk@gmail.com>
2023-06-22 23:54:26 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Bob&apos;s Sports Advertising</h1>
+        <p>For 20 years ...</p>
+        <Image src='' alt=''></Image>
+    </main>
+  )
+}
